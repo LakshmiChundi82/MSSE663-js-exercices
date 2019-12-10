0dcfe3d5f0a69ac6f05c2dde5d58bee0f2839e0f
LakshmiChundi82 <56930287+LakshmiChundi82@users.noreply.github.com>
2019-12-10 00:54:29 -0700

    Update array-destructuring.spec.ts

--- week5/src/array-destructuring.spec.ts
+++ week5/src/array-destructuring.spec.ts
@@ -1,26 +1,32 @@
 import {expect} from 'chai';
 import 'mocha';
-import {afterswap, others, wish} from './array-destructuring';
+import {fruits,a,b,afterswap,x,y,food,others,wish} from './array-destructuring';
 
 describe('Array destructuring tests', () => {
 
     it('#1 should have a variable for every element in the given array', () => {
-        console.log(afterswap);
-        expect(afterswap[0]).equal('banana')
+       
+        expect(a).equals(afterswap[0]);
+        expect(b).equals(afterswap[1]);
     });
 
     it('#2 should have a variable for the first 2 elements then spread the rest', () => {
-        const tempArray = ['chocolate', 'pears', 'oats', 'pizza'];
-        const isEqual = JSON.stringify(tempArray) === JSON.stringify(others);
-        expect(isEqual).equal(true);
-        console.log(tempArray);
+       
+      
+        expect(x).equals(food[0]),
+        expect(y).equals(food[1]),
+        expect(others).to.eql(['chocolate', 'pears', 'oats', 'pizza']);
+        
+ 
     });
 
 
     it('#3 should construct a statement from the given objarrayect', () => {
-        const value = (['hello', 'taylor!']);
-        const isEqual = JSON.stringify(value) === JSON.stringify(wish);
-        expect(isEqual).equal(true);
-        console.log(value);
+    
+        
+        expect(wish).to.be.eql(["hello", "taylor"]);
+       
     });
-});
+       
+    });
+
