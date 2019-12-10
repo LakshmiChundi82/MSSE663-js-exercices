b692913737d1e65cf3331879c05d12317622155f
LakshmiChundi82 <56930287+LakshmiChundi82@users.noreply.github.com>
2019-12-10 00:55:13 -0700

    Update array-destructuring.ts

--- week5/src/array-destructuring.ts
+++ week5/src/array-destructuring.ts
@@ -1,27 +1,35 @@
 // #1 Variable swapping
 export const fruits = ['apple', 'banana'];
 
-export let  [a,b] =fruits;
-
+export let  [a,b]=fruits;
 // Currently, I would get the following returns:
-// console.log(a); // 'apple'
-// console.log(b); // 'banana'
-export let afterswap=[a, b] = [b, a];
+console.log(a); // 'apple'
+console.log(b); // 'banana'
+
+export const afterswap =[a,b]=[b,a];
+
+// After swapping Values
+console.log(a); 
+console.log(b);
+
 
 
 // #2 Variables and Rest
 export const food = ['apple', 'banana', 'chocolate', 'pears', 'oats', 'pizza'];
-export let [, , ...others] =food;
-// console.log(a); // 'apple'
-// console.log(b); // 'banana'
-// console.log(others); // ['chocolate, 'pears', 'oats', 'pizza']
+export const [x,y, ...others] =food
+
+console.log(x); // 'apple'
+console.log(y); // 'banana'
+console.log(others); // ['chocolate, 'pears', 'oats', 'pizza']
 
 
 
 // #3 Array: Contruct a statement
 
-export const wish =['hello', 'taylor!'];
-export let [x, y]= wish;
+export const wish =['hello', 'taylor'];
+export let [wishes,name]=wish;
+console.log(wishes,",",name,"!");
 
 
 // 'hello, taylor!'
+
